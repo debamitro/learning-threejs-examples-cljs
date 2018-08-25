98cac4599fd7c6a17cc00f7c49dc059de4b9dd7a
Debamitro Chakraborti <debamitro@gmail.com>
2018-08-25 16:15:43 -0400

    reworking some parameters

--- src/cljthreetest1/core.cljs
+++ src/cljthreetest1/core.cljs
@@ -7,11 +7,11 @@
 (def sphere-color 0x7777FF)
 
 (defn create-renderer!
-  []
+  [color]
   ((fn [renderer]
      (do
 ;       (.setClearColorHex renderer)
-       (.setClearColor renderer (js/THREE.Color. renderer-color))
+       (.setClearColor renderer (js/THREE.Color. color))
        (.setSize renderer (.-innerWidth js/window) (.-innerHeight js/window))
        renderer))
    (js/THREE.WebGLRenderer.)))
@@ -25,10 +25,10 @@
   (js/THREE.PerspectiveCamera. 45 (/ (.-innerWidth js/window) (.-innerHeight js/window)) 0.1 1000))
 
 (defn create-sphere!
-  []
+  [radius widthSegments heightSegments color]
   (js/THREE.Mesh.
-    (js/THREE.SphereGeometry. 4 20 20)
-    (js/THREE.MeshBasicMaterial. (js-obj "color" sphere-color "wireframe" true))))
+    (js/THREE.SphereGeometry. radius widthSegments heightSegments)
+    (js/THREE.MeshBasicMaterial. (js-obj "color" color "wireframe" true))))
 
 (defn position-object!
   [object x y z]
@@ -39,10 +39,10 @@
     object))
 
 (defn create-plane!
-  []
+  [width height color]
   (js/THREE.Mesh.
-    (js/THREE.PlaneGeometry. 60 20)
-    (js/THREE.MeshBasicMaterial. (js-obj "color" plane-color))))
+    (js/THREE.PlaneGeometry. width height)
+    (js/THREE.MeshBasicMaterial. (js-obj "color" color))))
 
 (defn rotate-object!
   [object r]
@@ -51,10 +51,10 @@
     object))
 
 (defn create-cube!
-  [edgesize]
+  [edgesize color]
   (js/THREE.Mesh.
     (js/THREE.BoxGeometry. edgesize edgesize edgesize)
-    (js/THREE.MeshBasicMaterial. (js-obj "color" cube-color "wireframe" true))))
+    (js/THREE.MeshBasicMaterial. (js-obj "color" color "wireframe" true))))
 
 (defn add-output-to-html!
   [scene camera renderer]
@@ -67,9 +67,9 @@
 (defn create-objects!
   []
   [(js/THREE.AxisHelper. 20)
-   (rotate-object! (position-object! (create-plane!) 15 0 0) (* -0.5 (.-PI js/Math)))
-   (position-object! (create-sphere!) 20 4 2)
-   (position-object! (create-cube! 4) -4 3 0)])
+   (rotate-object! (position-object! (create-plane! 60 20 plane-color) 15 0 0) (* -0.5 (.-PI js/Math)))
+   (position-object! (create-sphere! 4 20 20 sphere-color) 20 4 2)
+   (position-object! (create-cube! 4 cube-color) -4 3 0)])
 
 (defn populate-scene!
   [scene objects]
@@ -82,4 +82,4 @@
   []
   (add-output-to-html! (populate-scene! (create-scene!) (create-objects!))
                        (position-object! (create-camera!) -30 40 30)
-                       (create-renderer!)))
+                       (create-renderer! renderer-color)))
